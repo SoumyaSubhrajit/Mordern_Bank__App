125216f6ac32542f011f0f669c55dfc7766a7b34
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2023-06-28 15:09:42 +0530

    Adding GetStarted code to the codeBase.

--- mordern_bank_app/src/components/GetStarted.jsx
+++ mordern_bank_app/src/components/GetStarted.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import styles from '../styles';
 
 function GetStarted() {
-  return <div>GetStarted</div>;
+  return (
+    <div
+      className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+    ></div>
+  );
 }
 
 export default GetStarted;
